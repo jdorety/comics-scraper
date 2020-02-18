95b602271c5a2d19eb4867a8c6bf02d49c8e6305
Jonathan Dorety <kyullog@gmail.com>
2020-02-18 11:14:30 -0800

    Successful data retrieval and response!

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
+const mongoose = require("./util/mongoose");
 
 const server = express();
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+  // we're connected!
+  console.log("whoop whoop");
+});
 
 server.use(cors());
 server.use(helmet());
 server.use(express.json());
 
-server.get("/", (req, res) => {
-  res.status(200).send({ message: "Hello world!" });
+server.get("/", async (req, res) => {
+  try {
+    const query = await db.model("Book").find();
+
+    res.status(200).send({ message: "Hello world!", test: query });
+  } catch (err) {
+    console.log(err);
+    res.status(400).send({ message: "DB not configured properly", error: err });
+  }
 });
 
 module.exports = server;
