654f596c8a57240e6939c8ccf2ef3628c2e1b01d
Jonathan Dorety <kyullog@gmail.com>
2020-02-18 19:31:10 -0800

    Create testing endpoints

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
+
 const db = require("./util/mongoose");
+const { Book } = require("./data/models");
 
 const server = express();
 
@@ -11,12 +13,23 @@ server.use(express.json());
 
 server.get("/", async (req, res) => {
   try {
-    const query = await db.model("Book").find();
-
-    res.status(200).send({ message: "Hello world!", test: query });
+    const books = await Book.find().exec();
+    res.status(200).send({ books: books });
   } catch (err) {
     console.log(err);
     res.status(400).send({ message: "DB not configured properly", error: err });
+  }
+});
+
+server.post("/test", async (req, res) => {
+  try {
+    const { book } = req.body;
+    comicBook = new Book(book);
+    const response = await comicBook.save();
+    res.status(201).json({ message: response });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "there were an error" });
   }
 });
 
